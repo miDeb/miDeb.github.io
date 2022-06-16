9e8701a99e112c69f7d56b7e5162fd000db938df
Michael Debertol <michael.debertol@gmail.com>
2022-06-16 17:47:39 +0200

    publish to docs/

--- .eleventy.js
+++ .eleventy.js
@@ -141,7 +141,7 @@ module.exports = function (eleventyConfig) {
       input: ".",
       includes: "_includes",
       data: "_data",
-      output: "_site",
+      output: "docs",
     },
   };
 };
